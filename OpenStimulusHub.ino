6d3bb4d33feafba489cddbf8fdb6560c1ff738ea
StimulusOnsethub <53412453+StimulusOnsethub@users.noreply.github.com>
2019-07-30 01:11:05 +0200

    Added license

--- OpenStimulusHub.ino
+++ OpenStimulusHub.ino
@@ -1,4 +1,30 @@
-// Copyright @ 2019 Jacob G. Martin and Charles Davis
+// Copyright @ 2019 Jacob G. Martin and Charles E. Davis.  All Rights Reserved.
+
+
+// Redistribution and use in source and binary forms, with or without
+// modification, are strictly prohibited.
+//
+// The names of Jacob G. Martin and Charles E. Davis may not be used to endorse or promote products
+// derived from or associated with this software without specific prior written permission.
+// 
+// No products may be derived from or associated with this software without specific prior written permission.
+
+// This software is provided "AS IS," without a warranty of any
+// kind. ALL EXPRESS OR IMPLIED CONDITIONS, REPRESENTATIONS AND
+// WARRANTIES, INCLUDING ANY IMPLIED WARRANTY OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE OR NON-INFRINGEMENT, ARE HEREBY
+// EXCLUDED. Jacob G. Martin and/or Charles E. Davis SHALL NOT BE LIABLE FOR ANY
+// DAMAGES OR LIABILITIES SUFFERED BY ANY ORGANIZATION OR ITS
+// LICENSEES AS A RESULT OF OR RELATING TO USE, MODIFICATION OR DISTRIBUTION OF THIS
+// SOFTWARE OR ITS DERIVATIVES. IN NO EVENT WILL Jacob G. Martin and/or Charles E. Davis BE LIABLE
+// FOR ANY LOST REVENUE, PROFIT OR DATA, OR FOR DIRECT, INDIRECT,
+// SPECIAL, CONSEQUENTIAL, INCIDENTAL OR PUNITIVE DAMAGES, HOWEVER
+// CAUSED AND REGARDLESS OF THE THEORY OF LIABILITY, ARISING OUT OF
+// THE USE OF OR INABILITY TO USE THIS SOFTWARE, EVEN IF Jacob G. Martin and/or Charles E. Davis HAS
+// BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGES AND THE USER OF THIS CODE
+// AGREES TO HOLD Jacob G. Martin and Charles E. Davis HARMLESS THEREFROM.
+
+
 #define FASTADC 1
 #define LIGHTSENABLED 1
 #define DIGITALWRITEFAST 1
